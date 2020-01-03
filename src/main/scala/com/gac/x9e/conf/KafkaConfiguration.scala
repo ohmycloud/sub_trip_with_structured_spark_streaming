2f6a27eb9194b2bd26e8e28fba6129657383b6a5
ohmycloud <ohmycloudy@gmail.com>
2020-01-03 14:59:11 +0800

    improve: remove redundancy code

--- src/main/scala/com/gac/x9e/conf/KafkaConfiguration.scala
+++ src/main/scala/com/gac/x9e/conf/KafkaConfiguration.scala
@@ -9,8 +9,4 @@ class KafkaConfiguration extends Serializable{
   private val config:                   Config = ConfigFactory.load()
   lazy    val kafkaConfig:              Config = config.getConfig("kafka")
   lazy    val bootstrapServers:         String = kafkaConfig.getString("common.bootstrap.servers")
-  lazy    val orginNaTopic:             String = kafkaConfig.getString("common.orginNaTopic")       // x9e 国标 kafka topic
-  lazy    val orginEnTopic:             String = kafkaConfig.getString("common.orginEnTopic")       // x9e 企标 kafka topic
-  lazy    val destinationNaTopic:       String = kafkaConfig.getString("common.destinationNaTopic") // xs6 国标 kafka topic
-  lazy    val destinationEnTopic:       String = kafkaConfig.getString("common.destinationEnTopic") // xs6 国标 kafka topic
 }
